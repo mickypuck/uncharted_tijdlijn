994fc7fdb16a9fc91957de86e8b6af0b22fea4c9
Micky Puck van Dartel <74116002+mickypuck@users.noreply.github.com>
2022-05-18 17:31:13 +0200

    Relics aangevuld + javascript en kleine aanpassingen

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,8 @@
 // JavaScript Document
-console.log("Howdy!");
+
+//Functie om de popup te laten zien en weer hiden na 3 sec
+function showRelic(id){
+ 	var element = document.getElementById(id);
+	element.classList.add('active');
+	setTimeout(function(){ element.classList.remove('active'); }, 3000);
+}
